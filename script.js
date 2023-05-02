19bdb75ed2cfd4a6abceff3fecb92bd5cb053d4d
Armelini <121030607+Armelini@users.noreply.github.com>
2023-05-02 19:31:33 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,49 @@
+const calc = document.getElementById("calc");
+
+function openCalc() {
+    calc.style.display = "block";
+}
+
+function closeCalc() {
+    calc.style.display = "none";
+}
+
+function calculate() {
+    const fNumber = parseInt(document.getElementById("fNumber").value);
+    const lNumber = parseInt(document.getElementById("lNumber").value);
+    const operation = document.getElementById("oper").value;
+    const resultNumber = document.getElementById("resultNumber");
+
+    let result = 0
+
+    switch (operation) {
+        case "add":
+            result = fNumber + lNumber
+            break;
+
+        case "sub":
+            result = fNumber - lNumber
+            break;
+
+        case "mult":
+            result = fNumber * lNumber
+            break;
+
+        case "div":
+            result = fNumber / lNumber
+            break;
+
+        default:
+            break;
+    }
+
+    resultNumber.innerHTML = result
+
+}
+
+function resetCalculator() {
+    document.getElementById("fNumber").value = 0;
+    document.getElementById("lNumber").value = 0;
+    document.getElementById("oper").value = "add";
+    document.getElementById("resultNumber").innerHTML = 0;
+}
